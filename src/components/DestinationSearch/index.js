ef676da23e7312435ac127d6d730cb06a3d71e2f
Manikalyan123 <kalyan.boddepalli1999@gmail.com>
2022-12-01 17:44:22 +0000

    destinationSearch

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,27 +1,35 @@
 // Write your code here
 import {Component} from 'react'
-import './App.js'
+import '../../App'
 import './index.css'
+import '../DestinationItem'
 
-const DestinationSearch = props => {
+class DestinationSearch extends Component {
   state = {searchInput: ''}
 
   onChangeSearch = event => {
     this.setState({searchInput: event.target.value})
   }
 
-  const {destinationsList} = props
-  return (
-    <div className="Cont">
-      <h1 className="heading">Destination Search</h1>
-      <input value={searchInput} type="search" onChange="" />
-      <ul className="searchItem-cont">
-        {destinationsList.map(eachItem => (
-          <searchItem destinationItem={eachItem} />
-        ))}
-      </ul>
-    </div>
-  )
+  render() {
+    const {destinationsList} = this.props
+    const {searchInput} = this.state
+    return (
+      <div className="Cont">
+        <h1 className="heading">Destination Search</h1>
+        <input
+          value={searchInput}
+          type="search"
+          onChange={this.onChangeSearch}
+        />
+        <ul className="searchItem-cont">
+          {destinationsList.map(eachItem => (
+            <SearchItem destinationItem={eachItem} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default DestinationSearch
